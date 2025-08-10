b715b76c4a95b4cafabb588fa56384b2ce41a7e5
Cho-job-heon <806jh806@naver.com>
2025-08-10 10:46:35 +0900

    sss

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ elif st.session_state.mode == 3:
     reference()
 elif st.session_state.mode == 2:  
     order_start()
+    
     new_ddk_select()
             
 menu_more_watching()                 
