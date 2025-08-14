0fe7375c1d8f5f9f104ae5e649bfed553763bb32
Cho-job-heon <806jh806@naver.com>
2025-08-14 15:47:39 +0900

    ss

--- code_dt/new_ddk_select.py
+++ code_dt/new_ddk_select.py
@@ -121,7 +121,7 @@ def new_ddk_select():
                 st.chat_message("assistant").write("✅주문이 완료되었습니다!")
                 st.write({step["name"]: st.session_state.get(step["name"]) for step in order_steps if step["name"] in st.session_state})
             if selected_order=='초기화':
-                st.session_state=0
+                current_order=0
                 selected_order=[]
                 step=[]
                 st.rerun()
