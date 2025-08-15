cdfa4d50ff15f0097f315474bc729817fdb317f3
Cho-job-heon <806jh806@naver.com>
2025-08-15 09:51:10 +0900

    ㄴㄴ

--- code_dt/new_ddk_select.py
+++ code_dt/new_ddk_select.py
@@ -138,7 +138,4 @@ def new_ddk_select():
                 st.session_state.order = 0
                 st.session_state.messages = []
                 st.rerun()
-            if selected_orders=='메뉴_추가':    
-                st.session_state.order = 0
-                st.write({step["name"]: st.session_state.get(step["name"]) for step in order_steps if step["name"] in st.session_state})
-                st.rerun()
+           
